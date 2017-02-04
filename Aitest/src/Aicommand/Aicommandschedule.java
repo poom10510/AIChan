b1ec41e51b1e3c1859d897b1b6ad16f4a3d94a24
Kitipoom <poom10510@gmail.com>
2017-02-04 23:01:09 +0700

    add schedule command

---
+++ Aitest/src/Aicommand/Aicommandschedule.java
@@ -0,0 +1,16 @@
+package Aicommand;
+
+public class Aicommandschedule extends Aicommand{
+
+	public Aicommandschedule(Aistatelist state, String key, String greeting) {
+		super(state, key, greeting);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String Active(String[] input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
